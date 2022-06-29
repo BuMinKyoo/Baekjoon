acce3b3729c9b4ad564711f9a2623e04600d1ad9
Min Kyoo Bu <39178978+BuMinKyoo@users.noreply.github.com>
2022-06-29 16:33:18 +0900

    Create 11654 아스키코드.cs

---
+++ CLASS 1/11654 아스키코드.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            char c = char.Parse(Console.ReadLine());
+            Console.WriteLine((int)c);
+        }
+    }
+}
